01e45bb7fd0bf7e5200ad8d8ecab9c8cd3f3a242
Michael <Michaelsergio02@gmail.com>
2023-10-23 17:50:07 -0300

    labs da aula 3 cisco devnet

---
+++ xml_yaml.py
@@ -0,0 +1,24 @@
+import xml.etree.ElementTree as ET
+import yaml
+tree = ET.parse('networking_config.xml')
+root = tree.getroot()
+
+network = {"routers": []}
+
+for router in root.findall('router'):   
+    router_data = {
+        "id": router.find('id').text,
+        "hostname": router.find('hostname').text,
+        "interfaces": [
+            {
+                "name": iface.find('name').text,
+                "ip_address": iface.find('ip_address').text,
+                "subnet_mask": iface.find('subnet_mask').text
+            }
+            for iface in router.findall('interfaces/interface')
+        ]
+    }
+    network['routers'].append(router_data)
+# Converter os dados em YAML e salvar
+with open('networking_config.yaml', 'w') as yaml_file:
+    yaml.dump(network, yaml_file, default_flow_style=False)
