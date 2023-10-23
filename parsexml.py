5a619d2af64ccdf0648793462643c9b94fd3dfbf
Michael <Michaelsergio02@gmail.com>
2023-10-23 13:49:49 -0300

    todos os labs realizados da aula3

---
+++ parsexml.py
@@ -0,0 +1,9 @@
+import xml.etree.ElementTree as ET
+tree = ET.parse('networking_config.xml')
+root = tree.getroot()
+for router in root.findall('router'):
+    router_name = router.find('hostname').text
+    for interface in router.find('interfaces').findall('interface'):
+        iface_name = interface.find('name').text
+        ip = interface.find('ip_address').text
+        print(f"{router_name}, Interface {iface_name} has IP: {ip}")
